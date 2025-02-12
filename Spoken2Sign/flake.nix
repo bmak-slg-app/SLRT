0066163a3b1cda35d341863613be0bc32a3ab688
Tommy Li <dev@stdx.space>
2025-02-12 16:05:11 +0800

    environment managment with uv2nix

--- Spoken2Sign/flake.nix
+++ Spoken2Sign/flake.nix
@@ -1,19 +1,102 @@
 {
-  description = "A very basic flake";
+  # flake modified from https://pyproject-nix.github.io/uv2nix/usage/hello-world.html
+  description = "Spoken2Sign";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-nix = {
+      url = "github:pyproject-nix/pyproject.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    uv2nix = {
+      url = "github:pyproject-nix/uv2nix";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pyproject-build-systems = {
+      url = "github:pyproject-nix/build-system-pkgs";
+      inputs.pyproject-nix.follows = "pyproject-nix";
+      inputs.uv2nix.follows = "uv2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      nixgl,
+      uv2nix,
+      pyproject-nix,
+      pyproject-build-systems,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
+
+      # Load a uv workspace from a workspace root.
+      # Uv2nix treats all uv projects as workspace projects.
+      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+
+      # Create package overlay from workspace.
+      overlay = workspace.mkPyprojectOverlay {
+        # Prefer prebuilt binary wheels as a package source.
+        # Sdists are less likely to "just work" because of the metadata missing from uv.lock.
+        # Binary wheels are more likely to, but may still require overrides for library dependencies.
+        sourcePreference = "wheel"; # or sourcePreference = "sdist";
+        # Optionally customise PEP 508 environment
+        # environ = {
+        #   platform_release = "5.10.65";
+        # };
+      };
+
+      # Extend generated overlay with build fixups
+      #
+      # Uv2nix can only work with what it has, and uv.lock is missing essential metadata to perform some builds.
+      # This is an additional overlay implementing build fixups.
+      # See:
+      # - https://pyproject-nix.github.io/uv2nix/FAQ.html
+      pyprojectOverrides = _final: _prev: {
+        # Implement build fixups here.
+        # Note that uv2nix is _not_ using Nixpkgs buildPythonPackage.
+        # It's using https://pyproject-nix.github.io/pyproject.nix/build.html
+      };
+
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ nixgl.overlay ];
+        };
+
+        # Use Python 3.12 from nixpkgs
+        python = pkgs.python310Full;
+
+        # Construct package set
+        pythonSet =
+          # Use base package set from pyproject.nix builders
+          (pkgs.callPackage pyproject-nix.build.packages {
+            inherit python;
+          }).overrideScope
+            (
+              lib.composeManyExtensions [
+                pyproject-build-systems.overlays.default
+                overlay
+                pyprojectOverrides
+              ]
+            );
       in
       {
         devShells.default = import ./shell.nix {
-          inherit pkgs;
+          inherit pkgs lib python;
         };
       }
     );
--- Spoken2Sign/shell.nix
+++ Spoken2Sign/shell.nix
@@ -1,29 +1,52 @@
 {
   pkgs ? import <nixpkgs> { },
+  lib,
+  python ? pkgs.python310Full,
 }:
 
 with pkgs;
 
 mkShell {
-  packages = with pkgs; [
-    blender
-    ffmpeg
-    uv
-    valkey
-  ];
-  NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
-    stdenv.cc.cc.lib
-    glib
-    zlib
-    xorg.libX11
-    xorg.libXrender
-    xorg.libXxf86vm
-    xorg.libXfixes
-    xorg.libXi
-    xorg.libSM
-    xorg.libICE
-    libxkbcommon
-  ];
-  NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
-  SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
+  packages =
+    [
+      python
+    ]
+    ++ (with pkgs; [
+      blender
+      ffmpeg
+      uv
+      valkey
+    ]);
+  env =
+    {
+      # Prevent uv from managing Python downloads
+      UV_PYTHON_DOWNLOADS = "never";
+      # Force uv to use nixpkgs Python interpreter
+      UV_PYTHON = python.interpreter;
+    }
+    // lib.optionalAttrs pkgs.stdenv.isLinux {
+      # Python libraries often load native shared objects using dlopen(3).
+      # Setting LD_LIBRARY_PATH makes the dynamic library loader aware of libraries without using RPATH for lookup.
+      LD_LIBRARY_PATH = lib.makeLibraryPath (
+        pkgs.pythonManylinuxPackages.manylinux1
+        ++ [
+          # stdenv.cc.cc.lib
+          # glib
+          # zlib
+          xorg.libX11
+          xorg.libXrender
+          xorg.libXxf86vm
+          xorg.libXfixes
+          xorg.libXi
+          xorg.libSM
+          xorg.libICE
+          libxkbcommon
+        ]
+      );
+      # NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
+      SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
+    };
+  shellHook = ''
+    unset PYTHONPATH
+  '';
 }
